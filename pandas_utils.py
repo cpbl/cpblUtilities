6f34d2406de7725775b4c590b32f54f69ad19a61
Chris Barrington-Leigh <cpbl@alum.mit.edu>
2018-02-20 19:35:47 -0800

    pandas_utils.py: adding df_first_differences_by_group()

--- pandas_utils.py
+++ pandas_utils.py
@@ -20,3 +20,18 @@ The first line must contain the column names.
     else:
         from io import StringIO
     return pd.read_table(StringIO(tss.strip('\n')))
+
+def df_first_differences_by_group(df, groupvar,yearvar):
+    """ This may be esoteric, but: rectangularize an index, and take first differences within each group.
+    """
+    # Assume index has been reset. Here are the data columns:
+    cols = [cc for cc in df.columns if cc not in [groupvar,yearvar]]
+    # Create a rectangular index of all group and year values:
+    blank =pd.DataFrame(index=pd.MultiIndex.from_product([
+        sorted(df[yearvar].unique()), sorted(df[groupvar].unique())], names= [yearvar,groupvar]))
+    dfsq = blank.join(df.set_index([yearvar, groupvar]))[cols].reset_index()
+    # Next, we can simply take differences:
+    dfsq.sort_values([groupvar,yearvar], inplace=True)
+    for var in cols:
+        dfsq['d_'+var] =dfsq.groupby([groupvar])[var].transform(lambda x: x.diff()) 
+    return dfsq
