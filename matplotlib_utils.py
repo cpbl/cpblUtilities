7c6a5bff8a076d1c215443601a9168c22a7b5cff
Chris Barrington-Leigh <cpbl@alum.mit.edu>
2018-03-10 09:12:33 -0800

    New module matplotlib_utils.py: for production figure sizing

--- matplotlib_utils.py
+++ matplotlib_utils.py
@@ -7,15 +7,39 @@ def prepare_figure_for_publication(ax=None,
         width_inches=None,
                                    height_cm=None,
         height_inches=None,
-        fontsize=9,
+                                   fontsize=None,
         fontsize_labels=None,
         fontsize_legend=None,
                                    fontsize_annotations =None,
         ):
+
     """
+    One reasonable option for making compact figures like for Science/Nature is to create everything at double scale. 
+    This works a little more naturally with Matplotlib's default line/axis/etc sizes.
+
+    Also, if you change sizes of, e.g. xticklabels and x-axis labels after they've been created, they will not necessarily be relocated appropriately.
+    So  you can call prepare_figure_for_publication with no ax/fig argument to set up figure defaults
+    prior to creating the figure in the first place.
+
     """
-    fuck
+
+    fig = ax.get_figure() 
+    if width_inches:
+        fig.set_figwidth(width_inches)
+        assert width_cm is None
+    if height_inches:
+        fig.set_figheight(height_inches)
+        assert height_cm is None
+    if width_cm:
+        fig.set_figwidth(width_cm/2.54)
+        assert width_inches is None
+    if height_cm:
+        fig.set_figheight(height_cm/2.54)
+        assert height_inches is None
+    
     #ax = plt.subplot(111, xlabel='x', ylabel='y', title='title')
-    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-                 ax.get_xticklabels() + ax.get_yticklabels()):
-        item.set_fontsize(fontsize)
+    for item in fig.findobj(plt.Text) + [ax.title, ax.xaxis.label, ax.yaxis.label] + ax.get_xticklabels() + ax.get_yticklabels():
+        if fontsize:
+            item.set_fontsize(fontsize)
+
+                        
